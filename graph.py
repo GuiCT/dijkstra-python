418eac3fcbb73cecc27d97d81d38c936ba3e2f0c
GuiCT <gtomiasi@gmail.com>
2022-08-15 22:21:47 -0300

    atualizando grafos: não-direcionados
    os grafos não deveriam ter sido implementados como direcionados,
    corrigindo isso

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-# Necessário utilizar grafos direcionados com peso
+# Necessário utilizar grafos NÃO direcionados com peso
 
 # Classe utilizada para implementar grafos
 class Graph:
@@ -11,8 +11,10 @@ class Graph:
         for _ in range(n):
             self.adj_list.append(list())
  
-        # Incluindo arestas para formar os grafos direcionados com peso
-        for (source, destination, value) in connections:
-            # Para o grafo de origem, adicionar destino e peso
-            self.adj_list[source].append((destination, value))
+        # Incluindo arestas para formar os grafos com peso
+        for (first, second, value) in connections:
+            # Para o grafo 1, adicionar destino (2) e peso
+            self.adj_list[first].append((second, value))
+            # Para o grafo 2, adicionar destino (1) e peso
+            self.adj_list[second].append((first, value))
  
