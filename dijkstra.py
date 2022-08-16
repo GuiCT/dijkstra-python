12b41653a56819efc24453397796d396ca7daa6b
GuiCT <gtomiasi@gmail.com>
2022-08-16 20:39:57 -0300

    atualizando Dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -1,34 +1,71 @@
 from graph import Graph
 from math import inf
 
-# Algoritmo de Dijsktra
-def dijkstra(graph : Graph):
+# Função que encontra vértice com menor distância
+# a partir do conjunto de vértices ainda não incluídos
+# na Árvore dos caminhos mínimos 
+def minDistance(distances, spt):
+    # Tamanho do grafo é inferido
+    size = len(distances)
+    # Distância mínima
+    min = -inf
+
+    # Realiza a busca pelo mínimo
+    for i in range(size):
+        if distances[i] < min and not spt[i]:
+            min = distances[i]
+            indexMin = i
+
+    return indexMin
+
+# Algoritmo de Dijsktra, executado a partir do grafo
+# de índice 'source'.
+def dijkstra(graph : Graph, source : int):
     # Quantidade de vértices
     size = len(graph.adj_list)
-    # Começando a partir do grafo 0
-    visited = [0]
-    # Para todos os vértices, se for adjacente ao 0, guardar a distância
-    # caso contrário, distância é infinita
-    # é infinita também em relação ao próprio vértice
-    distances = [inf] * size
-    conns = graph.adj_list[0]
-    for (ind, val) in conns:
-        distances[ind] = val
     
-    while len(visited) < size:
-        not_n = list(filter(lambda l: l[0] not in visited, conns))
-        min_nn = min(not_n, key=lambda l: l[1])
-        closest_next = min_nn[0]
-        dist_next = [inf] * size
-        conns_next = graph.adj_list[closest_next]
-        for (ind, val) in conns_next:
-            if ind not in visited:
-                dist_next[ind] = val
+    # Inicializando distâncias, distância da raiz para si mesma é 0
+    distances = [inf] * size
+    distances[source] = 0
+    # Árvore dos caminhos mínimos (Shortest Path Tree)
+    spt = [False] * size
+
+    # O Algoritmo de Dijkstra realiza um passo para cada vértice
+    # presente no grafo
+    for _ in range(size):
+        # Índice do vértice mais próximo dentre os
+        # vértices ainda não visitados.
+        closest = minDistance(distances, spt)
+
+        # Incluindo-o na SPT
+        spt[closest] = True
+
+        # Atualizando as distâncias para todos os
+        # vértices adjacentes ao vértice escolhido na
+        # etapa anterior. Essa distância só é atualizada
+        # se foi encontrada uma nova distância MENOR que
+        # a atual para um determinado vértice. Se o vértice
+        # destino está presente na SPT, o mesmo é ignorado.
+        # Lista de adjacência do vértice escolhido:
+        adjList = graph.adj_list[closest]
         for i in range(size):
-            distances[i] = min(distances[i], distances[closest_next] + dist_next[i])
-        last_visited = min(enumerate(distances), key=lambda l: l[1])[0]
-        visited.append(last_visited)
-        distances[last_visited] = inf
+            # Extrai o índice de todos os vértices adjacentes
+            # Verifica se i está presente na lista
+            adjs = zip(*adjList)[0]
+            isAdj = i in adjs
+            # Se é adjacente, extrair a distância
+            if isAdj:
+                dist_i = [item[1] for item in adjList if item[0] == i][0]
+            # Caso contrário, próxima iteração
+            else:
+                continue
+            # Nova distância
+            newDist = distances[closest] + dist_i
+            # Verifica se a distância nova é menor (ou igual) a atual
+            smallerDist = distances[i] > newDist
+            # Realizando a condicional comentada anteriormente
+            if isAdj and not spt[i] and smallerDist:
+                distances[i] = newDist
 
 # Testando
 if __name__ == '__main__':
