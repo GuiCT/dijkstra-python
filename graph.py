ebb80a1d88029dafaba9ad0068cfc482524b3fb8
GuiCT <gtomiasi@gmail.com>
2022-08-18 16:35:19 -0300

    utilizando dict no lugar de listas
    dicts para marcar adjacências

--- graph.py
+++ graph.py
@@ -4,17 +4,15 @@
 class Graph:
     def __init__(self, connections, n):
         # Lista de adjacência, com n vértices
-        # Cada elemento da lista é uma lista em si, visto
-        # que um único vértice pode ter mais de um vizinho,
-        # logo o número é variável.
+        # Cada elemento da lista é um dict, com chave
+        # indicando o destino e o valor o peso (distância)
         self.adj_list = list()
         for _ in range(n):
-            self.adj_list.append(list())
+            self.adj_list.append(dict())
  
         # Incluindo arestas para formar os grafos com peso
         for (first, second, value) in connections:
             # Para o grafo 1, adicionar destino (2) e peso
-            self.adj_list[first].append((second, value))
+            self.adj_list[first][second] = value
             # Para o grafo 2, adicionar destino (1) e peso
-            self.adj_list[second].append((first, value))
- 
+            self.adj_list[second][first] = value
